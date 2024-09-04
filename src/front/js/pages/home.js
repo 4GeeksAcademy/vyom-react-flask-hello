797fde650589dd28664f25fe105fb206121f8e29
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-09-04 20:26:44 +0000

    agreglando home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -10,7 +10,7 @@ export const Home = () => {
 
 	return (
 		<div style={{background: '#100E11'}} className=" w-100 h-100 mx-auto text-center text-white d-flex flex-column justify-content-center align-items-center">
-			<h1><p>Hola campeon, estas en casa</p><p>descansa en esta hoguera...</p></h1>
+			<h1>Hola campeon, estas en casa... <br/> descansa en esta hoguera...</h1>
 			<img src="https://steamuserimages-a.akamaihd.net/ugc/169289717866283092/C294AAC7AA3123748D598C3A16D07AEAAAD846C0/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"></img>
 		</div>
 	);
